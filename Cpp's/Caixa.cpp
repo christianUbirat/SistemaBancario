efa942fd3346530e06a8066420bc7cf0869ed541
Tranlei <89411420+Tranlei@users.noreply.github.com>
2021-08-28 16:39:30 -0300

    Add files via upload

---
+++ Cpp's/Caixa.cpp
@@ -0,0 +1,49 @@
+#include "Caixa.h"
+
+// Construtor da classe Caixa
+Caixa::Caixa(double = 0.0){
+    ifstream Caixa;
+    Caixa.open("Dinheiros.txt" , ios::in);
+    Caixa>>Qtd_dinheiro;
+    if(Caixa.is_open()){
+        while(!Caixa.eof()){
+            Caixa>>Notas.frist;
+            if(Caixa.eof())
+                break;
+            Caixa>>Notas.second;
+        }
+    }
+    Caixa.close();
+}
+
+// Funcionalidades do caixa
+void Caixa::gerenciar(void){
+    string opcao;
+    do{
+        cout<<"----------Menu(Gerenciamento)----------"<<endl
+            <<"O que deseja fazer ?"<<endl
+            <<"(1)- Abastecer   ."<<endl
+            <<"(2)- Retirar     ."<<endl
+            <<"(3)- Sair do menu."<<endl;
+        cin<<opcao;
+        if(opcao == "1")
+            abastecer();
+        else if(opcao == "2")
+            remover();
+        else
+            cout<<"Opção inválida ,por favor tente novamente ."endl;
+    }while(opcao != "3");
+}
+
+// Construtor da classe Caixa
+Caixa::~Caixa(){
+    ofstream Caixa("Dinheiros.txt", ios::trunc);
+    Caixa<<Qtd_dinheiro;
+    if(Caixa.is_open()){
+        for(int indice = 0 ; indice < Notas.size() ; indice++){
+            Caixa<<nota[i].first
+                <<nota[i].second;
+        }
+    }
+    Caixa.close();
+}
