9c44f8b0ea2bdb998453aa3937b433188d429b5c
Tranlei <89411420+Tranlei@users.noreply.github.com>
2021-09-09 13:47:14 -0300

    moren

--- Prototipos/Usuario.h
+++ Prototipos/Usuario.h
@@ -1,7 +1,7 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
-class Usuario:public Usuario{
+class Usuario{
 private :
 	string nome, senha;
 	int numero_conta;
@@ -10,7 +10,8 @@ private :
 	vector<string>historico;
 public :
 	// Construtor
-    Usuario(string = "", string = "", int = 0, double = 0.0, double 0.0, boll = false);
+	//Caminhao(int = 0, string = "", double = 0.0 ,string = "" ,string = "", int = 0, int = 0, int = 0);
+    Usuario(string = "",string = "", int = 0, double = 0.0, double = 0.0, bool = false);
 
     // Metodos Set
     void set_nome(string);
@@ -18,7 +19,7 @@ public :
     void set_numero_conta(int);
     void set_debito(double);
     void set_credito(double);
-    void set_privilegio(bool)
+    void set_privilegio(bool);
 
     // Metodos Get
     string get_nome(void)const;
