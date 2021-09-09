ebea62061ad05b70f630d38fc2e42a2f81ca971c
Tranlei <89411420+Tranlei@users.noreply.github.com>
2021-09-09 13:43:34 -0300

    dnv

--- Cpp's/Caixa.cpp
+++ Cpp's/Caixa.cpp
@@ -1,10 +1,10 @@
 #include "Caixa.h"
 
 // Construtor da classe Caixa
-Caixa::Caixa(double = 0.0){
+Caixa::Caixa(){
     ifstream Caixa;
     Caixa.open("Dinheiros.txt" , ios::in);
-    Caixa>>qtd_dinheiro;
+    Caixa>>Qtd_dinheiro;
     if(Caixa.is_open()){
         pair <int,int> auxiliar;
         while(!Caixa.eof()){
@@ -27,23 +27,26 @@ void Caixa::gerenciar(void){
             <<"(1)- Abastecer   ."<<endl
             <<"(2)- Retirar     ."<<endl
             <<"(3)- Sair do menu."<<endl;
-        cin<<opcao;
+        cin>>opcao;
         if(opcao == "1")
-            abastecer();
+            cout<<"p";
+            //abastecer();
         else if(opcao == "2")
-            remover();
+            cout<<"i";
+            //remover();
         else
-            cout<<"Opção inválida ,por favor tente novamente ."endl;
+            cout<<"Opção inválida ,por favor tente novamente ."<<endl;
     }while(opcao != "3");
 }
 
 // Construtor da classe Caixa
 Caixa::~Caixa(){
     ofstream Caixa("Dinheiros.txt", ios::trunc);
-    Caixa<<qtd_dinheiro;
+    Caixa<<Qtd_dinheiro;
     if(Caixa.is_open()){
-        for(int indice = 0 ; indice < notas.size() ; indice++)
-            Caixa<<notas[indice].first<<endl<<notas[indice].second<<endl;
+        for(int indice = 0 ; indice < notas.size() ; indice++){
+            Caixa<<notas[indice].first<<notas[indice].second;
+        }
     }
     Caixa.close();
 }
