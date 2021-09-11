406bf4ec6d95ad75289f89b7f78094dcea719bf9
Tranlei <89411420+Tranlei@users.noreply.github.com>
2021-09-11 20:27:22 -0300

    .h atualizada

--- Prototipos/Caixa.h
+++ Prototipos/Caixa.h
@@ -1,25 +1,32 @@
 #ifndef CAIXA_H
 #define CAIXA_H
 
-#include "Banco.cpp"
-
 class Caixa{
 private :
     double Qtd_dinheiro;
-    //Biblioteca ultillit
     //Pair<valor da nota, Qtd de nota>
     vector<pair<int,int>>notas;
-public :
-    // Construtor
-    Caixa();
+    int saldo;
 
+public :
+    // Construtor(False padrão,True private)
+    Caixa(int);
     // Funcionalidades do caixa
+    // Função da adimistração
     void gerenciar(void);
-    void abastecer(double);
-    void remover(double);
+    void abastecer(void);
+    void remover(void);
+    //Processos
+    int proceso_abastecer(void);
+    int proceso_remocao(void);
+    // Função do's usuários
+    int deposito(void);
+    int saque(void);
+    void contabilizar(void);
 
     // Destrutor
-    ~Caixa(void);
+    void salvar(void);
+    ~Caixa();
 };
 
 #endif
